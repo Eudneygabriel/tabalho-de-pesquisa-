c82acd562fceb2ccd63bac7f893c3f9859cd52f6
Eudneygabriel <eudneygabriel@epfundao.edu.pt>
2024-02-13 11:34:39 +0000

    primero commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var body = document.body;
+
+  body.style.backgroundImage =
+    "url('https://png.pngtree.com/element_our/md/20180327/md_041012ce20cacd1d0d1d7b86dd18d28f.jpg')";
+});
